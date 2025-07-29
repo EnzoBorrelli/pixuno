33665996cbfdf0d6286bf80267da75efc44f6e35
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-07-29 07:06:29 -0300

    added spread logic

--- app/components/deck.tsx
+++ app/components/deck.tsx
@@ -1,14 +1,39 @@
-import { Container } from "@pixi/react";
+import { Container, Graphics } from "@pixi/react";
 import { iDeck } from "~/interfaces/game";
 import Card from "./card";
 
-export default function Deck({ coords, id, cards }: iDeck) {
+export default function Deck({ coords, id, cards, angle }: iDeck) {
+  const CARD_WIDTH = 64;
+  const DECK_WIDTH = 400;
+  const cardCount = cards.length;
+
+  //dinamic spacing
+  const idealSpacing = 40;
+  const maxSpacing = (DECK_WIDTH - CARD_WIDTH) / (cardCount - 1 || 1);
+  const spacing = Math.min(idealSpacing, maxSpacing);
+
+  //total width of cards spread
+  const spreadWidth = (cardCount - 1) * spacing + CARD_WIDTH;
+
+  //starting x to center the deck
+  const cardOrigin = (DECK_WIDTH - spreadWidth) / 2;
+
   return (
-    <Container x={coords.x} y={coords.y}>
+    <Container x={coords.x} y={coords.y} angle={angle} interactive={true}>
+      <Graphics
+        draw={(g) => {
+          g.clear();
+          g.beginFill(0xff0000, 0.3); // Red with some transparency
+          g.drawRect(0, 0, 400, 100);
+          g.endFill();
+        }}
+        x={0}
+        y={0}
+      />
       {cards.map((card, index) => (
         <Card
-        key={index}
-          coords={card.coords}
+          key={index}
+          coords={{ x: cardOrigin + index * spacing, y: 0 }}
           type={card.type}
           color={card.color}
           isFlipped={card.isFlipped}
