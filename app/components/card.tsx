5cbdd4954a254becdd100613b4cb4e07037e15aa
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-07-29 07:06:34 -0300

    Update card.tsx

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -4,7 +4,7 @@ import { CARD_COLOR, CARD_TYPE } from "~/enums/cards";
 import { iCard } from "~/interfaces/game";
 
 export default function Card({
-  coords,
+  coords = { x: 0, y: 0 },
   type = CARD_TYPE.UNO,
   color = CARD_COLOR.RED,
   isFlipped,
@@ -17,14 +17,14 @@ export default function Card({
     <Sprite
       ref={spriteRef}
       texture={texture}
-      x={coords.x}
-      y={coords.y + hoverY}
+      x={coords.x+32}
+      y={coords.y+50 + hoverY}
       width={64}
       height={100}
       angle={angle}
       scale={{ x: 1, y: 1 }}
       anchor={0.5}
-      interactive={true}
+      interactive={!isFlipped}
       pointerover={() => setHover(true)}
       pointerout={() => setHover(false)}
     />
