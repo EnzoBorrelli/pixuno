2a6bedfb0ddbd5b921de03d2177d675e07f15ef9
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-07-29 14:04:10 -0300

    Update game.ts

--- app/interfaces/game.ts
+++ app/interfaces/game.ts
@@ -3,6 +3,7 @@ import { CARD_TYPE, CARD_COLOR } from "../enums/cards";
 export type Coords = { x: number; y: number };
 
 export interface iCard {
+  id: string;
   coords?: Coords;
   type: CARD_TYPE;
   color: CARD_COLOR;
@@ -11,7 +12,7 @@ export interface iCard {
 }
 
 export interface iDeck {
-  coords: Coords;
+  position: Coords;
   id: string;
   cards: iCard[];
   angle?:number
