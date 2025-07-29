a779031364c9fb0863b06590cf9de806c49299eb
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-07-29 14:04:25 -0300

    Update cardDeckExample.ts

--- app/utils/cardDeckExample.ts
+++ app/utils/cardDeckExample.ts
@@ -2,75 +2,54 @@ import { CARD_TYPE, CARD_COLOR } from "~/enums/cards";
 
 export const CardDeckExample = [
   {
+    id: "",
     type: CARD_TYPE.OCHO,
     color: CARD_COLOR.GREEN,
     isFlipped: false,
   },
+  { id: "", type: CARD_TYPE.UNO, color: CARD_COLOR.RED, isFlipped: false },
+  { id: "", type: CARD_TYPE.BLOCK, color: CARD_COLOR.BLUE, isFlipped: false },
   {
-    type: CARD_TYPE.UNO,
-    color: CARD_COLOR.RED,
-    isFlipped: false,
-  },
-  {
-    type: CARD_TYPE.BLOCK,
-    color: CARD_COLOR.BLUE,
-    isFlipped: false,
-  },
-  {
+    id: "",
     type: CARD_TYPE.PLUS_TWO,
     color: CARD_COLOR.YELLOW,
     isFlipped: false,
   },
+  { id: "", type: CARD_TYPE.SEIS, color: CARD_COLOR.YELLOW, isFlipped: false },
   {
-    type: CARD_TYPE.SEIS,
-    color: CARD_COLOR.YELLOW,
-    isFlipped: false,
-  },
-  {
+    id: "",
     type: CARD_TYPE.MULTICOLOR,
     color: CARD_COLOR.BASE,
     isFlipped: false,
   },
-  {
-    type: CARD_TYPE.DOS,
-    color: CARD_COLOR.YELLOW,
-    isFlipped: false,
-  },
-]
+  { id: "", type: CARD_TYPE.DOS, color: CARD_COLOR.YELLOW, isFlipped: false },
+];
 export const RivalCardDeckExample = [
+  { id: "", type: CARD_TYPE.REVERSE, color: CARD_COLOR.GREEN, isFlipped: true },
+  { id: "", type: CARD_TYPE.REVERSE, color: CARD_COLOR.RED, isFlipped: true },
+  { id: "", type: CARD_TYPE.REVERSE, color: CARD_COLOR.BLUE, isFlipped: true },
   {
-    type: CARD_TYPE.REVERSE,
-    color: CARD_COLOR.GREEN,
-    isFlipped: true,
-  },
-  {
-    type: CARD_TYPE.REVERSE,
-    color: CARD_COLOR.RED,
-    isFlipped: true,
-  },
-  {
-    type: CARD_TYPE.REVERSE,
-    color: CARD_COLOR.BLUE,
-    isFlipped: true,
-  },
-  {
+    id: "",
     type: CARD_TYPE.REVERSE,
     color: CARD_COLOR.YELLOW,
     isFlipped: true,
   },
   {
+    id: "",
     type: CARD_TYPE.REVERSE,
     color: CARD_COLOR.YELLOW,
     isFlipped: true,
   },
   {
+    id: "",
     type: CARD_TYPE.REVERSE,
     color: CARD_COLOR.YELLOW,
     isFlipped: true,
   },
   {
+    id: "",
     type: CARD_TYPE.REVERSE,
     color: CARD_COLOR.YELLOW,
     isFlipped: true,
   },
-]
+];
