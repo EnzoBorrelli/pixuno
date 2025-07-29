56430e6420cad79f1f13be2bc994a21b38b6c57f
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-07-29 14:03:41 -0300

    added deck slices

--- app/components/game.tsx
+++ app/components/game.tsx
@@ -1,26 +1,65 @@
-import { Stage } from "@pixi/react";
+import { Stage, TilingSprite } from "@pixi/react";
 import Deck from "./deck";
 import { CardDeckExample, RivalCardDeckExample } from "~/utils/cardDeckExample";
-import CentralDeck from "./centralDeck";
-
-
+import DiscardDeck from "./discardDeck";
+import PickupDeck from "./pickupDeck";
+import { useSelector } from "react-redux";
+import { RootState } from "~/store";
 
 export default function Game() {
+  const player = useSelector((state: RootState) => state.player);
+  const aiLeft = useSelector((state: RootState) => state.aiLeft);
+  const aiUp = useSelector((state: RootState) => state.aiUp);
+  const aiRight = useSelector((state: RootState) => state.aiRight);
+  const discard = useSelector((state: RootState) => state.discard);
+  const pickup = useSelector((state: RootState) => state.pickup);
   return (
     <Stage
       width={800}
       height={600}
       options={{
-        backgroundColor: 0xaaaaaa,
+        backgroundColor: 0xa65232,
         antialias: false,
         autoDensity: true,
       }}
     >
-      <Deck coords={{x:200,y:450}} id="player1" cards={CardDeckExample}/> 
-      <Deck coords={{x:150,y:100}} id="player1" cards={RivalCardDeckExample} angle={90}/> 
-      <Deck coords={{x:650,y:500}} id="player1" cards={RivalCardDeckExample} angle={270}/> 
-      <Deck coords={{x:600,y:150}} id="player1" cards={RivalCardDeckExample} angle={180}/> 
-      <CentralDeck coords={{x:340,y:240}} id="player1" cards={CardDeckExample}/> 
+      <TilingSprite
+        tilePosition={{ x: 0, y: 0 }}
+        image="/assets/images/wood-dark.png"
+        width={800}
+        height={600}
+        alpha={0.6}
+        
+      />
+      <Deck position={player.position} id={player.id} cards={CardDeckExample} />
+      <Deck
+        position={aiLeft.position}
+        id={aiLeft.id}
+        cards={RivalCardDeckExample}
+        angle={aiLeft.angle}
+      />
+      <Deck
+        position={aiUp.position}
+        id={aiUp.id}
+        cards={RivalCardDeckExample}
+        angle={aiUp.angle}
+      />
+      <Deck
+        position={aiRight.position}
+        id={aiRight.id}
+        cards={RivalCardDeckExample}
+        angle={aiRight.angle}
+      />
+      <DiscardDeck
+        position={discard.position}
+        id={discard.id}
+        cards={CardDeckExample}
+      />
+      <PickupDeck
+        position={pickup.position}
+        id={pickup.id}
+        cards={RivalCardDeckExample}
+      />
     </Stage>
   );
 }
---
+++ app/store/deckSliceFactory.ts
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { iCard, iDeck } from "~/interfaces/game";
+
+const createDeckSlice = (
+  name: string,
+  position: { x: number; y: number },
+  angle: number
+) => {
+  const initialState: iDeck = {
+    position: position,
+    id: name,
+    cards: [],
+    angle: angle,
+  };
+  return createSlice({
+    name: name,
+    initialState,
+    reducers: {
+      addCard: (state, action) => {
+        const card: iCard = action.payload;
+        state.cards.push(card);
+      },
+      removeCard: (state, action) => {
+        const index = state.cards.findIndex(
+          (card) => card.id === action.payload.id
+        );
+        if (index !== -1) {
+          state.cards.splice(index, 1);
+        }
+      },
+      moveCard: (state, action) => {
+        const { index, newCoords } = action.payload;
+        if (state.cards[index]) {
+          state.cards[index].coords = newCoords;
+        }
+      },
+      clearDeck: (state) => {
+        state.cards = [];
+      },
+    },
+  });
+};
+
+export default createDeckSlice;
