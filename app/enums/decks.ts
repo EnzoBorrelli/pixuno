5c5d780bc5e02906d0c58cf24522c8a4258113ff
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-07-29 14:04:03 -0300

    Create decks.ts

---
+++ app/enums/decks.ts
@@ -0,0 +1,17 @@
+export const DECK_POSITION = {
+  PLAYER: { x: 200, y: 450 },
+  AI_LEFT: { x: 150, y: 100 },
+  AI_UP: { x: 600, y: 150 },
+  AI_RIGHT: { x: 650, y: 500 },
+  DISCARD: { x: 420, y: 240 },
+  PICKUP: { x: 260, y: 240 },
+};
+
+export const DECK_ANGLE = {
+  PLAYER: 0,
+  AI_LEFT: 90,
+  AI_UP: 180,
+  AI_RIGHT: 270,
+  DISCARD: 0,
+  PICKUP: 0,
+};
