d0ffa56cbe01164d1589e16d3a88f1e36e702ada
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-07-30 14:15:58 -0300

    Create shuffleDeck.ts

---
+++ app/scripts/decks/shuffleDeck.ts
@@ -0,0 +1,10 @@
+import { iCard } from "~/interfaces/game";
+
+export function shuffleDeck(deck: iCard[]): iCard[] {
+  const shuffledDeck = [...deck];
+  for (let i = shuffledDeck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
+  }
+  return shuffledDeck;
+}
