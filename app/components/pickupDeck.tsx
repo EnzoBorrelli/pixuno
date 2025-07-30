e1024f1d8c47b7e53989f8a5e3fd6a00c31f405d
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-07-30 14:16:44 -0300

    added anglespread logic

--- app/components/pickupDeck.tsx
+++ app/components/pickupDeck.tsx
@@ -3,6 +3,8 @@ import { iDeck } from "~/interfaces/game";
 import Card from "./card";
 
 export default function PickupDeck({ position, cards, id }: iDeck) {
+  const maxSpread = 10;
+  const halfSpread = maxSpread / 2;
   return (
     <Container x={position.x} y={position.y} width={120} height={120}>
       <Graphics
@@ -15,18 +17,22 @@ export default function PickupDeck({ position, cards, id }: iDeck) {
         x={0}
         y={0}
       />
-      {cards.map((card, index) => (
-        <Card
-          id={card.id}
-          key={index}
-          coords={{ x: 30, y: 10 }}
-          angle={index}
-          type={card.type}
-          color={card.color}
-          isFlipped={card.isFlipped}
-          deckID={index+1 === cards.length ? id : ""}
-        />
-      ))}
+      {cards.map((card, index) => {
+        const t = cards.length > 1 ? index / (cards.length - 1) : 0.5;
+        const angle = t * maxSpread - halfSpread;
+        return (
+          <Card
+            id={card.id}
+            key={card.id}
+            coords={{ x: 30, y: 10 }}
+            angle={angle}
+            type={card.type}
+            color={card.color}
+            isFlipped={card.isFlipped}
+            deckID={index + 1 === cards.length ? id : ""}
+          />
+        );
+      })}
     </Container>
   );
 }
