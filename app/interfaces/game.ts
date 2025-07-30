edaf8212ccbbf862a411f2fedb66ad5f7a1d61b1
Endy® <106992127+EnzoBorrelli@users.noreply.github.com>
2025-07-30 14:16:18 -0300

    added game interface

--- app/interfaces/game.ts
+++ app/interfaces/game.ts
@@ -1,3 +1,4 @@
+import { GAME_FLOW, GAME_STATES, GAME_TURNS } from "~/enums/game";
 import { CARD_TYPE, CARD_COLOR } from "../enums/cards";
 
 export type Coords = { x: number; y: number };
@@ -8,12 +9,19 @@ export interface iCard {
   type: CARD_TYPE;
   color: CARD_COLOR;
   isFlipped: boolean;
-  angle?:number
+  angle?: number;
 }
 
 export interface iDeck {
   position: Coords;
   id: string;
   cards: iCard[];
-  angle?:number
+  angle?: number;
+}
+
+export interface iGame {
+  state: GAME_STATES;
+  current_turn: GAME_TURNS;
+  flow: GAME_FLOW;
+  turn_timer: number;
 }
